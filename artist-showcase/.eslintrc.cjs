9790d862f68af9ed38ecda8a25dc70f7319541ea
saajidkamran <mohamed.shiras@harver.com>
2025-11-14 10:11:54 +0530

    Initial commit: Last.fm Music Explorer setup

---
+++ artist-showcase/.eslintrc.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["react", "react-hooks", "@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  settings: {
+    react: { version: "detect" },
+  },
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "@typescript-eslint/no-unused-vars": ["error"],
+  },
+};
